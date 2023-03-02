7e1bdb5fc5bf9e9c5c76a7a01d2751fbcd761e2c
IvandryPage <ivandry.89@gmail.com>
2023-03-02 13:56:05 +0700

    Add solution for Utopian Tree solution

---
+++ ProblemSolving/UtopianTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class UtopianTree : IBaseClass
+	{
+		public void Invoke()
+		{
+			int n = 5;
+			utopianTree(n);
+		}
+
+		public static int utopianTree(int n)
+		{
+			int initialHeight = 1;
+
+			for (int i = 1; i <= n; i++)
+			{
+				if (i % 2 == 1)
+				{
+					initialHeight *= 2;
+					continue;
+				}
+
+				initialHeight += 1;
+			}
+
+			return initialHeight;
+		}
+	}
+}
