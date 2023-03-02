16bf0d733127d57ca1ff9bb78e51326df44ff7f7
IvandryPage <ivandry.89@gmail.com>
2023-03-02 13:29:22 +0700

    Add solution for Sales by Match problem

---
+++ ProblemSolving/SalesByMatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class SalesByMatch : IBaseClass
+	{
+		public void Invoke()
+		{
+			int n = 20;
+			List<int> ar = new List<int>();
+			sockMerchant(n, ar);
+		}
+		public static int sockMerchant(int n, List<int> ar)
+		{
+			List<int> arr = ar.Distinct().ToList();
+			ar.Sort();
+
+			int pairsCounter = 0;
+			for (int i = 0; i < arr.Count; i++)
+			{
+				int totalMatchedNumber = 0;
+				for (int j = 0; j < ar.Count; j++)
+				{
+					if (arr[i] == ar[j])
+						totalMatchedNumber += 1;
+				}
+
+				if (totalMatchedNumber % 2 == 0)
+					pairsCounter += totalMatchedNumber / 2;
+				else
+					pairsCounter += Convert.ToInt16(Math.Floor((double)totalMatchedNumber / 2));
+			}
+
+			return pairsCounter;
+		}
+	}
+}
