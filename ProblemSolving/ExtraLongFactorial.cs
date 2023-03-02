a49057a9e5aa9fdab08f1957490adf65e0d8a543
IvandryPage <ivandry.89@gmail.com>
2023-03-02 14:56:16 +0700

    Add solution for Extra Long Factorial problem

---
+++ ProblemSolving/ExtraLongFactorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class ExtraLongFactorial : IBaseClass
+	{
+		public void Invoke()
+		{
+			int n = 90;
+			extraLongFactorials(n);
+		}
+
+		public static void extraLongFactorials(int n)
+		{
+			Console.WriteLine(factorial(n));
+		}
+
+		private static BigInteger factorial(BigInteger n)
+		{
+			if (n <= 1)
+				return 1;
+			return n * factorial(n - 1);
+		}
+	}
+}
