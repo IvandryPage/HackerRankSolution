68f08052a5c3d3b09f192949218a2f725ee16163
IvandryPage <ivandry.89@gmail.com>
2023-03-02 13:03:29 +0700

    Add solution for Grading Students problem

--- CallerApp/Program.cs
+++ CallerApp/Program.cs
@@ -6,7 +6,7 @@ namespace Caller
 	{
 		static void Main(string[] args)
 		{
-			var problemSolver = new TimeConversion();
+			var problemSolver = new GradingStudents();
 			problemSolver.Invoke();
 
 			Console.ReadKey(true);
---
+++ ProblemSolving/GradingStudents.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class GradingStudents : IBaseClass
+	{
+		public void Invoke()
+		{
+			List<int> grades = new List<int>();
+			gradingStudents(grades);
+		}
+
+		public static List<int> gradingStudents(List<int> grades)
+		{
+			List<int> roundedGrades = new List<int>();
+			foreach (int grade in grades)
+			{
+				if (grade < 38 || ( grade + 5 ) % 5 < 3)
+				{
+					roundedGrades.Add(grade);
+				}
+				else
+				{
+					int roundedGrade = grade + ( 5 - ( grade % 5 ) );
+					roundedGrades.Add(roundedGrade);
+				}
+			}
+
+			return roundedGrades;
+		}
+	}
+}
