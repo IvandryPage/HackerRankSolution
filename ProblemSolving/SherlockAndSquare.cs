25f168d588c8cabeee5ad7f57e8f52dde7abc922
IvandryPage <ivandry.89@gmail.com>
2023-03-02 14:57:45 +0700

    Add solution for Sherlock and Squares problem

---
+++ ProblemSolving/SherlockAndSquare.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class SherlockAndSquare : IBaseClass
+	{
+		public void Invoke()
+		{
+			int a = 0;
+			int b = 2;
+			squares(a, b);
+		}
+
+		public static int squares(int a, int b)
+		{
+			int counter = 0;
+			for (int i = 1; i * i <= b; i++)
+			{
+				if (i * i >= a)
+					counter++;
+			}
+			return counter;
+		}
+	}
+}
