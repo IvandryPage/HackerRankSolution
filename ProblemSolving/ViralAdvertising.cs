36264d75efd0744db6199d6bbb61d028eb81d376
IvandryPage <ivandry.89@gmail.com>
2023-03-02 14:43:31 +0700

    Add solution for Viral Advertising problem

---
+++ ProblemSolving/ViralAdvertising.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class ViralAdvertising : IBaseClass
+	{
+		public void Invoke()
+		{
+			viralAdvertising(5);
+		}
+
+		public static int viralAdvertising(int n)
+		{
+			double liked = 2;
+			int cumulative = Convert.ToInt32(liked);
+
+			for (int i = 1; i < n; i++)
+			{
+				double shared = liked * 3;
+				liked = Math.Floor((double)( shared / 2 ));
+				cumulative += Convert.ToInt32(liked);
+			}
+
+			return cumulative;
+		}
+	}
+}
