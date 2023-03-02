0082dd205d8cfe95a412e4799917fd785643526f
IvandryPage <ivandry.89@gmail.com>
2023-03-02 13:55:53 +0700

    Add solution for designer pdf viewer solution

---
+++ ProblemSolving/DesignerPDFViewer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class DesignerPDFViewer : IBaseClass
+	{
+		public void Invoke()
+		{
+			List<int> h = new List<int>();
+			string word = "hackerrank";
+			designerPdfViewer(h, word);
+		}
+
+		public static int designerPdfViewer(List<int> h, string word)
+		{
+			int maxHeight = 0;
+
+			for (int i = 0; i < word.Length; i++)
+			{
+				int index = Convert.ToInt32(word[i]) - Convert.ToInt32('a');
+				if (maxHeight < h[index])
+					maxHeight = h[index];
+			}
+
+			return maxHeight * word.Length;
+		}
+	}
+}
