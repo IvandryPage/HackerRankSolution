f8b508bb352096ec4eeb12be860f1215ce94e311
IvandryPage <ivandry.89@gmail.com>
2023-03-02 13:00:26 +0700

    Add solution for Time Conversion problem

--- CallerApp/Program.cs
+++ CallerApp/Program.cs
@@ -6,7 +6,7 @@ namespace Caller
 	{
 		static void Main(string[] args)
 		{
-			var problemSolver = new BirthdayCakeCandles();
+			var problemSolver = new TimeConversion();
 			problemSolver.Invoke();
 
 			Console.ReadKey(true);
---
+++ ProblemSolving/TimeConversion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class TimeConversion : IBaseClass
+	{
+		public void Invoke()
+		{
+			string s = "12:00PM";
+			timeConversion(s);
+		}
+
+		public static string timeConversion(string s)
+		{
+			string period = s.Substring(8, 2);
+			int h = Convert.ToInt16(s.Substring(0, 2));
+
+			if (period.ToLower() == "pm")
+			{
+				if (h == 24)
+					h = 0;
+				else if (h != 12)
+					h += 12;
+			}
+			else
+			{
+				if (h == 12)
+					h = 0;
+			}
+
+			return h.ToString("00") + s.Substring(2, 6);
+		}
+	}
+}
