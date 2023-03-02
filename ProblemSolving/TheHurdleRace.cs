aa6c4d1bc57fa40cdb91b0c1713f23365eb3a016
IvandryPage <ivandry.89@gmail.com>
2023-03-02 13:52:01 +0700

    Add solution for The Hurdle Race problem

---
+++ ProblemSolving/TheHurdleRace.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class TheHurdleRace : IBaseClass
+	{
+		public void Invoke()
+		{
+			int k = 0;
+			List<int> height = new List<int>();
+			hurdleRace(k, height);
+		}
+
+		public static int hurdleRace(int k, List<int> height)
+		{
+			int maxHurdle = 0;
+
+			for (int i = 0; i < height.Count; i++)
+			{
+				if (maxHurdle < height[i])
+					maxHurdle = height[i];
+			}
+
+			// Or simply use maxHurdle = height.Max();
+
+			return ( k < maxHurdle ) ? maxHurdle - k : 0;
+		}
+	}
+}
