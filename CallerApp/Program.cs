89461c5eb7e6c98dbcae29b0811b055774c30883
IvandryPage <ivandry.89@gmail.com>
2023-03-02 12:58:31 +0700

    Add solution for Birthday Cake Candles problem

--- CallerApp/Program.cs
+++ CallerApp/Program.cs
@@ -6,7 +6,7 @@ namespace Caller
 	{
 		static void Main(string[] args)
 		{
-			var problemSolver = new MinMaxSum();
+			var problemSolver = new BirthdayCakeCandles();
 			problemSolver.Invoke();
 
 			Console.ReadKey(true);
---
+++ ProblemSolving/BirthdayCakeCandles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class BirthdayCakeCandles : IBaseClass
+	{
+		public void Invoke()
+		{
+			List<int> candles = new List<int>();
+			birthdayCakeCandles(candles);
+		}
+
+		public static int birthdayCakeCandles(List<int> candles)
+		{
+			candles.Sort();
+			int counter = 0;
+
+			for (int i = 0; i < candles.Count; i++)
+			{
+				if (candles[i] == candles[candles.Count - 1])
+					counter++;
+			}
+			return counter;
+		}
+	}
+}
