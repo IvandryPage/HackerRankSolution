038d8c93292d1d2f73ffc304aae37a69ccdb426a
IvandryPage <ivandry.89@gmail.com>
2023-03-02 14:45:08 +0700

    Add solution for Save the Prisoner problem

---
+++ ProblemSolving/SaveThePrisoner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class SaveThePrisoner : IBaseClass
+	{
+		public void Invoke()
+		{
+			int n = 1;
+			int m = 1;
+			int s = 1;
+
+			saveThePrisoner(n, m, s);
+		}
+
+		public static int saveThePrisoner(int n, int m, int s)
+		{
+			int result = ( s + m - 1 ) % n;
+			return ( result != 0 ) ? result : n;
+		}
+	}
+}
