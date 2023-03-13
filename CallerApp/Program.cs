b708be068bf1719c632a6d6cab1f188817d429a0
IvandryPage <ivandry.89@gmail.com>
2023-03-13 18:58:24 +0700

    Add solution for Equalize Array problem

--- CallerApp/Program.cs
+++ CallerApp/Program.cs
@@ -6,7 +6,7 @@ namespace Caller
 	{
 		static void Main(string[] args)
 		{
-			var problemSolver = new JumpingOnClouds();
+			var problemSolver = new EqualizeArray();
 			problemSolver.Invoke();
 
 			Console.ReadKey(true);
---
+++ ProblemSolving/EqualizeArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class EqualizeArray : IBaseClass
+	{
+		public void Invoke()
+		{
+			List<int> arr = new List<int> { 1, 2, 2, 3 };
+
+			// expected : 2
+			equalizeArray(arr);
+		}
+
+		public static int equalizeArray(List<int> arr)
+		{
+			List<int> numbersInArray = arr.ToHashSet().ToList();
+
+			int mostFrequent = 0;
+			foreach(int num in numbersInArray)
+			{
+				int count = arr.Count(x => x == num);
+
+				if (count > mostFrequent)
+					mostFrequent = count;
+			}
+
+			return arr.Count - mostFrequent; // minimum delete operation
+		}
+	}
+}
