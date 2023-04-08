4f819195fb8d306ce039061e17147d635c0dbb48
IvandryPage <ivandry.89@gmail.com>
2023-04-09 04:44:42 +0700

    Add solution for ModifiedKaprekarNumber.cs

--- CallerApp/Program.cs
+++ CallerApp/Program.cs
@@ -6,7 +6,7 @@ namespace Caller
 	{
 		static void Main(string[] args)
 		{
-			var problemSolver = new AcmIcpcTeam();
+			var problemSolver = new ModifiedKaprekarNumber();
 			problemSolver.Invoke();
 
 			Console.ReadKey(true);
---
+++ ProblemSolving/ModifiedKaprekarNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+	public class ModifiedKaprekarNumber : IBaseClass
+	{
+		public void Invoke()
+		{
+			int p = 1;
+			int q = 100;
+			// Expected Result : 1 9 45 55 99
+			KaprekarNumbers(p, q);
+		}
+
+		public static void KaprekarNumbers(int p, int q)
+		{
+			bool FoundAtleastOneKaprekar = false;
+			for(int i = p; i <= q; i++)
+			{
+				int d = i.ToString().Length;
+				string squaredNum = Math.Pow(i, 2).ToString();
+				string l = squaredNum.Substring(0, squaredNum.Length - d);
+				string r = squaredNum.Substring(squaredNum.Length - d);
+
+				int lNum = ( !string.IsNullOrEmpty(l) ) ? Convert.ToInt32(l) : 0;
+				int rNum = ( !string.IsNullOrEmpty(r) ) ? Convert.ToInt32(r) : 0;
+
+				if ( lNum + rNum == i )
+				{
+					if (!FoundAtleastOneKaprekar)
+						FoundAtleastOneKaprekar = true;
+					Console.Write($"{i} ");
+				}
+			}
+
+			if (!FoundAtleastOneKaprekar)
+				Console.WriteLine("INVALID RANGE");
+		}
+	}
+}
